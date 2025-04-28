5f319e356a4c61c9c6b456402f4a0e64b7673b5b
ranepriyanshu <priyanshurane09@gmail.com>
2025-04-28 12:39:43 +0530

    dashboard routes

--- Backend/server.js
+++ Backend/server.js
@@ -43,6 +43,7 @@ const authRoutes = require("./routes/authRoutes");
 
 const incomeRoutes = require("./routes/incomeRoutes");
 const expenseRoutes = require("./routes/expenseRoutes");
+const dashboardRoutes = require("./routes/dashboardRoutes");
 
 const app = express();
 
@@ -60,6 +61,7 @@ app.use(express.json()); // 👈 Parsing JSON body
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/income", incomeRoutes);
 app.use("/api/v1/expense", expenseRoutes);
+app.use("/api/v1/dashboard", dashboardRoutes); 
 
 
 
